1870be28fdb88a782c334642cd528619e25793c0
extraterrestria1 <kvasnevskyy@gmail.com>
2018-01-14 21:00:49 +0200

    Add files via upload

---
+++ Formula1RaceResult/Interfaces/GrandPrixResults.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+
+
+
+namespace Formula1RaceResult.Interfaces
+{
+
+
+
+    public class GrandPrixResults
+    {
+
+        public string GrandPrixResultsId { get; set; }
+
+        public string Pos { get; set; }
+
+        public string No { get; set; }
+
+        public string Driver { get; set; }
+
+        public string Car { get; set; }
+
+        public string Laps { get; set; }
+
+        [Column("Time & Retired")]
+        public string TimeRetired { get; set; }
+
+        public string Pts { get; set; }
+
+        public GrandPrixResults(string grandPrixResultsId, DataRow dataRow)
+        {
+
+            Pos = (string)dataRow["Pos"];
+            No = (string)dataRow["No"];
+            Driver = (string)dataRow["Driver"];
+            Car = (string)dataRow["Car"];
+            Laps = (string)dataRow["Laps"];
+            TimeRetired = (string)dataRow["Time & Retired"];
+            Pts = (string)dataRow["Pts"];
+
+            GrandPrixResultsId = $"{grandPrixResultsId}\\{Driver}";
+        }
+
+    }
+
+
+
+    public class GrandPrix
+    {
+
+        public string GrandPrixId { get; set; }
+
+        public string GrandPrixResultsTitle { get; set; }
+
+    }
+
+
+
+}
---
+++ Formula1RaceResult/Interfaces/Interfaces.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+
+
+
+namespace Formula1RaceResult.Interfaces
+{
+
+
+
+    public interface IFormula1RaceResult
+    {
+
+        string CurrentRaceResultsArchiveId { get; set; }
+
+        DataTable GrandPrixResults { get; set; }
+
+    }
+
+
+
+    public interface IFormula1RaceResultHtmlParser
+    {
+
+        string ParseRaceResultDataTableFromUrl(string url, DataTable dataTable);
+
+    }
+
+
+
+    public interface IFormula1RaceResultDbManipulalor
+    {
+
+        string ConnectionString { get; set; }
+
+        bool IsRaceResultsExistsInDbByIdCheck(string raceResultsArchiveId);
+
+        string ErrorMessageSavingDataToDb(string raceResultsArchiveId, string raceResultsArchiveTitle, DataTable grandPrixResults);
+
+    }
+
+
+
+}
